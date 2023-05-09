15434839f62be7a7be0a10863c22c5d4c74c4de3
ramonabreu-data <cdevabreu@gmail.com>
2023-05-09 10:50:16 -0300

    Altt configs mysql-hibernate

--- eventoapp/src/main/java/com/eventoapp/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -1,6 +1,30 @@
 package com.eventoapp.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
+//import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+///import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Evento implements Serializable {
+
+    private  static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long codigo;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
 
     private String nome;
     private String local;
